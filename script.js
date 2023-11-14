da8314d1e9e674fab187439debb23982093b6d70
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-11-14 16:51:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,52 +2,47 @@ let carrito = [];
 let totalCarrito = 0;
 
 function agregarAlCarrito(nombre, precio) {
-    const producto = { nombre, precio };
-    carrito.push(producto);
+    carrito.push({ nombre, precio });
     totalCarrito += precio;
-
-    // Actualiza el contador del carrito
-    actualizarContadorCarrito();
 
     actualizarCarrito();
 }
 
 function actualizarContadorCarrito() {
-    const contadorCarrito = document.getElementById('contador-carrito');
-    contadorCarrito.textContent = carrito.length;
+    document.getElementById('contador-carrito').textContent = carrito.length;
 }
 
 function actualizarCarrito() {
     const listaCarrito = document.getElementById('lista-carrito');
     const totalCarritoElemento = document.getElementById('total-carrito');
 
-    listaCarrito.innerHTML = '';
-
-    carrito.forEach(producto => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
+    listaCarrito.innerHTML = carrito.map(producto => `
+        <li>
             <span>${producto.nombre}</span>
             <span class="precio">$${producto.precio.toFixed(2)}</span>
-        `;
-        listaCarrito.appendChild(listItem);
-    });
+        </li>
+    `).join('');
 
     totalCarritoElemento.textContent = totalCarrito.toFixed(2);
+    actualizarContadorCarrito();
 }
 
 function comprar() {
-    // Simular la lógica de compra (enviar datos al servidor, procesar pago, etc.)
     alert("¡Compra realizada con éxito! Gracias por tu compra.");
     
-    // Reiniciar el carrito
+    reiniciarCarrito();
+}
+
+function reiniciarCarrito() {
     carrito = [];
     totalCarrito = 0;
-
-    actualizarContadorCarrito();
     actualizarCarrito();
 }
 
 function mostrarCarrito() {
-    // Agrega aquí la lógica para mostrar el carrito
-    alert("Mostrar carrito"); // Puedes reemplazar esto con la lógica real
+    alert("Mostrar carrito");
 }
+
+// Ejemplo de uso:
+agregarAlCarrito('Producto 1', 10);
+agregarAlCarrito('Producto 2', 20);
